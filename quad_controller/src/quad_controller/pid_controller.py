74241ec13bf38059ca82ce4afc7d1ec5e90d44ae
Stephan Becker <beckererno+github@gmail.com>
2017-10-28 17:46:45 +0200

    Add PID Controller

--- quad_controller/src/quad_controller/pid_controller.py
+++ quad_controller/src/quad_controller/pid_controller.py
@@ -6,34 +6,89 @@
 
 class PIDController:
     def __init__(self, kp = 0.0, ki = 0.0, kd = 0.0, max_windup = 10):
-        #TODO
-        pass
+        self.kp_ = float(kp)
+        self.ki_ = float(ki)
+        self.kd_ = float(kd)
+
+        # Set max wind up
+        self.max_windup_ = float(max_windup)
+
+        # Store relevant data
+        self.last_timestamp_ = 0.0
+        self.set_point_ = 0.0
+        self.error_sum_ = 0.0
+        self.last_error_ = 0.0
+
+        # Control effort history
+        self.u_p = [0]
+        self.u_i = [0]
+        self.u_d = [0]
+
     def reset(self):
-        #TODO
-        pass
+        self.set_point_ = 0.0
+        self.kp_ = 0.0
+        self.ki_ = 0.0
+        self.kd_ = 0.0
+        self.error_sum_ = 0.0
+        self.last_timestamp_ = 0.0
+        self.last_error_ = 0
+        self.last_last_error_ = 0
+        self.last_windup_ = 0.0
 
     def setTarget(self, target):
-        #TODO
-        pass
+        self.set_point_ = float(target)
 
     def setKP(self, kp):
-        #TODO
-        pass
+        self.kp_ = float(kp)
 
     def setKI(self, ki):
-        #TODO
-        pass
+        self.ki_ = float(ki)
 
     def setKD(self, kd):
-        #TODO
-        pass
+        self.kd_ = float(kd)
 
     def setMaxWindup(self, max_windup):
-        #TODO
-        pass
+        self.max_windup_ = int(max_windup)
 
     def update(self, measured_value, timestamp):
-        #TODO
-        pass
+        delta_time = timestamp - self.last_timestamp_
+        if delta_time == 0:
+            # Delta time is zero
+            return 0
 
+        # Calculate the error
+        error = self.set_point_ - measured_value
 
+        # Set the last_timestamp_
+        self.last_timestamp_ = timestamp
+
+        # Sum the errors
+        self.error_sum_ += error * delta_time
+
+        # Find delta_error
+        delta_error = error - self.last_error_
+
+        # Update the past error
+        self.last_error_ = error
+
+        # Address max windup
+        self.error_sum_ = max(-self.max_windup_, min(self.max_windup_, self.error_sum_))
+
+        # Proportional error
+        p = self.kp_ * error
+
+        # Integral error
+        i = self.ki_ * self.error_sum_
+
+        d = self.kd_ * delta_error / delta_time
+
+        # Set the control effort
+        u = p + i + d
+
+        # Here we are storing the control effort history for post control
+        # observations.
+        self.u_p.append(p)
+        self.u_i.append(i)
+        self.u_d.append(d)
+
+        return u
